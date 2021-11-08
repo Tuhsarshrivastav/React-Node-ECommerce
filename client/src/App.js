1764eada3723a2ab644c031b369d52a624f13537
Tusharshrivastav <ts3657@gmail.com>
2021-11-08 08:31:49 +0530

    added rating comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import data from "./data";
 import { BrowserRouter, Route } from "react-router-dom";
+import Product from "./components/Product";
 function App() {
   const openMenu = () => {
     document.querySelector(".sidebar").classNameList.add("open");
@@ -26,34 +27,7 @@ function App() {
           <div>
             <div className="row center">
               {data.products.map((product) => (
-                <div key={product._id} className="card">
-                  <a href={`/product/${product._id}`}>
-                    <img className="medium" src={product.image} alt="product" />
-                  </a>
-                  <div className="card-body">
-                    <a href={`/product/${product._id}`}>
-                      <h2>{product.name}</h2>
-                    </a>
-                    <div className="rating">
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                    </div>
-                    <div className="price">${product.price}</div>
-                  </div>
-                </div>
+                <Product key={product._id} product={product} />
               ))}
             </div>
           </div>
