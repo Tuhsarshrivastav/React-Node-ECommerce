55fa6e15f5d91ce9ecae0f5fa49db1b1e51d73e1
Tusharshrivastav <ts3657@gmail.com>
2021-11-10 07:18:13 +0530

    import user data into db

--- server.js
+++ server.js
@@ -1,25 +1,42 @@
-import express from 'express';
-import data from './data.js';
-
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import mongoose from "mongoose";
+import data from "./data.js";
+import userRouter from "./routers/userRouter.js";
 const app = express();
 
+const Connect = async () => {
+  await mongoose.connect(process.env.db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("Database is Connected");
+};
+Connect();
 
-app.get('/api/products/:id', (req, res) => {
+app.get("/api/products/:id", (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
   if (product) {
     res.send(product);
   } else {
-    res.status(404).send({ message: 'Product Not Found' });
+    res.status(404).send({ message: "Product Not Found" });
   }
 });
 
-app.get('/api/products', (req, res) => {
+app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 
-app.get('/', (req, res) => {
-  res.send('Server is ready');
+app.use("/api/users", userRouter);
+app.get("/", (req, res) => {
+  res.send("Server is ready");
 });
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
